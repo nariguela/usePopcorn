b736f18b0e1cb0826841e25968ce831d060dba16
Pedro <pedrodc3048@gmail.com>
2025-05-14 21:45:32 -0300

    Adiciona foco no campo de busca ao pressionar Enter e utiliza useRef para referência do input

--- src/components/Navbar/SearchBar.jsx
+++ src/components/Navbar/SearchBar.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 export default function SearchBar({ query, setQuery }) {
+  // useEffect(() => {
+  //   const el = document.querySelector(".search");
+  //   el.focus();
+  // }, []);
+
+  const inputElement = useRef(null);
+
+  useEffect(() => {
+    function callback(e) {
+      if (document.activeElement === inputElement.current) return;
+
+      if (e.code === "Enter") {
+        inputElement.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+    return () => document.addEventListener("keydown", callback);
+  }, [setQuery]);
+
   return (
     <input
       className="search"
@@ -8,6 +29,7 @@ export default function SearchBar({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
     />
   );
 }
